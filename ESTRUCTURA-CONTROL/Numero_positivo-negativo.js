cb57015f4f2c7ffeecefaffbee084ac9a7a4b112
LupitaMlqs18 <guadmala108@gmail.com>
2024-07-01 18:46:24 -0600

    Add files via upload
    Son los trabajos realizados el dia 01 de julio

---
+++ ESTRUCTURA-CONTROL/Numero_positivo-negativo.js
@@ -0,0 +1,23 @@
+//Numeros positivos o negativo
+
+//IF
+let num = 5;
+if(num>0){
+    console.log("Positivo"); //si la condicion es correcta se cumple
+}
+//if-else
+
+if(num<0){
+    console.log("Positivo");
+} else {
+    console.log("Negativo");
+}
+//Bloque de codigo
+if(num>1 > Positivo){
+    console.log("El numero es positivo");
+} else {
+    console.log("El numero es negativo");
+} else {
+    //Bloque de codigo
+    console.log("Error");
+}
---
+++ ESTRUCTURA-CONTROL/Numeros_Positivos_Negativo.js
@@ -0,0 +1,17 @@
+//Numeros positivos o negativo
+
+//IF
+let num = 0;
+if(num>0){
+    console.log("Positivo"); //si la condicion es correcta se cumple
+}
+//if-else
+
+if(num<0){
+    console.log("negativo");
+} else if (num === 0) {
+    console.log("neutro");
+
+}
+//Bloque de codigo
+
